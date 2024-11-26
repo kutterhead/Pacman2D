564056f2c2004bba52a1d065301e6780553c4234
Kutterhead <esseanx@gmail.com>
2024-11-26 19:34:57 +0100

    rasycast disparando

--- Assets/enemigo.cs
+++ Assets/enemigo.cs
@@ -11,6 +11,7 @@ public class enemigo : MonoBehaviour
 
     public bool[] direccionesObstruidas;
 
+    private int direccionMovActual = 0;
     void Start()
     {
         detectaColisiones();
@@ -20,10 +21,10 @@ public class enemigo : MonoBehaviour
     void Update()
     {
         //permite detectar como iuun colider
-        //detectaColisiones();
+        detectaColisiones();
         //solo para visualizar en vista de escena
         
-       // transform.Translate(transform.right * speed * Time.deltaTime);
+        //transform.Translate(transform.right * speed * Time.deltaTime);
     }
 
     public void detectaColisiones()
@@ -34,94 +35,53 @@ public class enemigo : MonoBehaviour
         
         Vector3 direccion;
 
-        //direccion = transform.right;
-        // Debug.DrawRay(transform.position, direccion * rayDistance, Color.green, .1f); Debug.DrawRay(transform.position, transform.right * rayDistance, Color.green, .1f);
-        //direccion = transform.up;
-        //Debug.DrawRay(transform.position, direccion * rayDistance, Color.green, .1f); Debug.DrawRay(transform.position, transform.right * rayDistance, Color.green, .1f);
+
 
         direccion = transform.up;
-        Debug.DrawRay(transform.position, direccion * rayDistance, Color.green, .1f); Debug.DrawRay(transform.position, transform.right * rayDistance, Color.green, .1f);
+        
 
         int contador = 4;
-
-        //while (contador>0)
-        //{
-
-        //    print("Iteración: " +contador);
-
-        //    contador--;
-        //    direccion = transform.up;
-        //    Debug.DrawRay(transform.position, direccion * rayDistance, Color.green, .1f); Debug.DrawRay(transform.position, transform.right * rayDistance, Color.green, .1f);
-
-        //}
-
-
-
-        for (int i = 0; i < 4; i++)
-        {
-
-
-            print("Iteración: " + i);
-
-           
-            direccion = transform.up;
-            Debug.DrawRay(transform.position, direccion * rayDistance, Color.green, .1f); Debug.DrawRay(transform.position, transform.right * rayDistance, Color.green, .1f);
-
-
-
-        }
-
-
-
-
-            return;
 
 
         for (int i = 0; i < direccionesObstruidas.Length;i++)
         {
 
-            
-            //switch (i)
-            //{
-            //    case 0:
-            //        direccion = transform.up;
-            //        break;
-            //    case 1:
-            //        direccion = -transform.up;
-            //        break;
-            //    case 2:
-            //        direccion = -transform.right;
-            //        break;
-            //    default:
 
-            //        direccion = transform.right;
-            //        break;
+            switch (i)
+            {
+                case 0:
+                    direccion = transform.up;
+                    break;
+                case 1:
+                    direccion = -transform.up;
+                    break;
+                case 2:
+                    direccion = -transform.right;
+                    break;
+                default:
 
-            //}
+                    direccion = transform.right;
+                    break;
 
-
-
-            direccion = transform.right;
+            }
+           
 
 
-            Debug.DrawRay(transform.position, direccion * rayDistance, Color.green, .1f); Debug.DrawRay(transform.position, transform.right * rayDistance, Color.green, .1f);
-            direccion = transform.right;
-
-            RaycastHit2D hit = Physics2D.Raycast(direccion, transform.up, rayDistance);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direccion, rayDistance);
 
             if (hit.collider != null)
             {
                 Debug.Log(direccion);
-                Debug.DrawRay(transform.position, direccion * rayDistance, Color.red, .1f);
+                Debug.DrawRay(transform.position, direccion * rayDistance, Color.red);
                 Debug.Log(hit.collider.tag);
-                speed = -speed;
+                //speed = -speed;
                 direccionesObstruidas[i] = true;
             }
 
             else
             {
                 direccionesObstruidas[i] = false;
-                Debug.DrawRay(transform.position, direccion * rayDistance, Color.green, .1f);
+                Debug.DrawRay(transform.position, direccion * rayDistance, Color.green);
             }
 
         }
